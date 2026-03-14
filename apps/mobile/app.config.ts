de01ee6c4693c7807aa0a48885d6e45c0828adf1
김용민 <dydals3440@gmail.com>
2026-03-15 01:53:50 +0900

    chore(mobile): v1.0.2 버전 업데이트

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -15,7 +15,7 @@ interface EnvironmentConfig {
 
 const PROJECT_SLUG = 'aido';
 const OWNER = 'aido-team';
-const VERSION = '1.0.1';
+const VERSION = '1.0.2';
 
 const APP_NAME = 'Aido';
 const BUNDLE_IDENTIFIER = 'com.aido.mobile';
